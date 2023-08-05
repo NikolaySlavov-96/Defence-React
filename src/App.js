2f17d93cf6a1532ce055b8d1927a034e81b67d1a
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-08-05 13:15:08 +0300

    create paths in whole project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 // import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Home } from './components/Home/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-      </header>
-    </div>
-  );
+    return (
+        <Routes>
+            <Route path='*' element={<h1>404 Error Content</h1>} />
+            <Route path='/' element={<Home />} />
+        </Routes>
+    );
 }
 
 export default App;
