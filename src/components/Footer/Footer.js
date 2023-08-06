6daad59564aeb878a5f5d3c842a423f9ced62bd8
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-08-06 18:33:15 +0300

    created footer view

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.module.css';
+
+export const Footer = () => {
+    return (
+        <footer>
+            <div className="shadow">
+                <p>Designed by Internet Space</p>
+                <p>React Defence of Nikolay Slavov 2023/2024</p>
+            </div>
+        </footer>
+    );
+}
