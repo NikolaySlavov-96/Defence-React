1f957149bc65d48145621481df15d8985a9e989e
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-08-07 17:52:45 +0300

    added more path of app file

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Home } from './components/Home/Home';
 import { About } from './components/About/About';
+import { Header } from './components/Header/Header';
+import { Footer } from './components/Footer/Footer';
+import { PageNotFound } from './components/PageNotFound/PageNotFound';
+import { Catalog } from './components/Product/Catalog/Catalog';
+import { Detail } from './components/Product/Details/Detail';
+import { Edit } from './components/Product/Edit/Edit';
+import { Create } from './components/Product/Create/Create';
+import { Delete } from './components/Product/Delete/Delete';
 
 function App() {
     return (
-        <Routes>
-            <Route path='*' element={<h1>404 Error Content</h1>} />
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-        </Routes>
+        <>
+            <Header />
+
+            <Routes>
+                <Route path='*' element={<PageNotFound />} />
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/product/catalog' element={<Catalog />} />
+                <Route path='/product/create' element={<Create />} />
+                <Route path='/product/detail/:id' element={<Detail />} />
+                <Route path='/product/edit/:id' element={<Edit />} />
+                <Route path='/product/delete/:id' element={<Delete />} />
+            </Routes>
+
+            <Footer />
+        </>
     );
 }
 
