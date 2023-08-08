7866260fd854002c7d8f6ca51f5d31b487f1a480
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-08-08 19:04:50 +0300

    created and other handlers

--- src/services/product.js
+++ src/services/product.js
@@ -3,16 +3,21 @@ import { requesterFactory } from './requester';
 export const productServiceFactory = (token) => {
     const request = requesterFactory(token);
 
-    const getProducts = async () => {
-        return request.get('/source/products');
-    }
+    const getProducts = async () => request.get('/source/products');
 
-    const getProduct = async (id) => {
-        return request.get('' + id);
-    }
+    const getProduct = async (id) => request.get('/source/product/' + id);
+
+    const createProduct = async (data) => request.post('/source/product', data);
+
+    const editProduct = async (id, data) => request.put('/source/product/' + id, data);
+
+    const deleteProduct = async (id) => request.remove('/source/product/' + id);
 
     return {
         getProducts,
         getProduct,
+        createProduct,
+        editProduct,
+        deleteProduct,
     }
 }
