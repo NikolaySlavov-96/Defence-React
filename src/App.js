40d28d699d8cb39bf34944720e65fe18a925c665
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-08-08 14:57:00 +0300

    using product context in app file

--- src/App.js
+++ src/App.js
@@ -11,22 +11,29 @@ import { Detail } from './components/Product/Details/Detail';
 import { Edit } from './components/Product/Edit/Edit';
 import { Create } from './components/Product/Create/Create';
 import { Delete } from './components/Product/Delete/Delete';
+import { Login } from './components/Auth/Login/Login';
+import { Register } from './components/Auth/Register/Register';
+import { ProductProvider } from './contexts/ProductContext';
 
 function App() {
     return (
         <>
             <Header />
 
-            <Routes>
-                <Route path='*' element={<PageNotFound />} />
-                <Route path='/' element={<Home />} />
-                <Route path='/about' element={<About />} />
-                <Route path='/product/catalog' element={<Catalog />} />
-                <Route path='/product/create' element={<Create />} />
-                <Route path='/product/detail/:id' element={<Detail />} />
-                <Route path='/product/edit/:id' element={<Edit />} />
-                <Route path='/product/delete/:id' element={<Delete />} />
-            </Routes>
+            <ProductProvider>
+                <Routes>
+                    <Route path='*' element={<PageNotFound />} />
+                    <Route path='/' element={<Home />} />
+                    <Route path='/auth/login' element={<Login />} />
+                    <Route path='/auth/register' element={<Register />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/product/catalog' element={<Catalog />} />
+                    <Route path='/product/create' element={<Create />} />
+                    <Route path='/product/detail/:id' element={<Detail />} />
+                    <Route path='/product/edit/:id' element={<Edit />} />
+                    <Route path='/product/delete/:id' element={<Delete />} />
+                </Routes>
+            </ProductProvider>
 
             <Footer />
         </>
