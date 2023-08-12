6c1d20aa6765d94864c9cc71b27f8ebe7b3d94ce
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-08-12 16:35:56 +0300

    enhanced useForm return and errors

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,10 +1,21 @@
 import { useState } from "react";
 
-export const useForm = (initialValue, onSubmitHandler) => {
+export const useForm = (initialValue, onSubmitHandler, errorTarget) => {
     const [values, setValue] = useState(initialValue);
+    const [errors, setErrors] = useState(initialValue);
 
     const changeHandler = (e) => {
         setValue(state => ({ ...state, [e.target.name]: e.target.value }));
+
+        if (errorTarget[e.target.name] && errorTarget[e.target.name][0] === 'required') {
+            if (values[e.target.name].length <= 1) {
+                setErrors(state => ({ ...state, [e.target.name]: `${e.target.name} is required` }))
+            } else if (errorTarget[e.target.name]?.[1] >= values[e.target.name].length) {
+                setErrors(state => ({ ...state, [e.target.name]: `Minimal length is ${errorTarget[e.target.name][1]}` }))
+            } else {
+                setErrors(state => ({ ...state, [e.target.name]: '' }))
+            }
+        }
     }
 
     const onSubmit = (e) => {
@@ -23,5 +34,6 @@ export const useForm = (initialValue, onSubmitHandler) => {
         changeHandler,
         onSubmit,
         changeValue,
+        errors
     }
 }
