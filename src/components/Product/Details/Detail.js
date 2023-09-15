8f94a0e434fd8c717fc18f0ae02036a062b11d6b
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-09-15 21:17:28 +0300

    adding checker for user or owner

--- src/components/Product/Details/Detail.js
+++ src/components/Product/Details/Detail.js
@@ -3,12 +3,14 @@ import style from './Detail.module.css';
 import { useEffect, useState } from 'react';
 import { productServiceFactory } from '../../../services/product';
 import { useService } from '../../../hooks/useService';
+import { useAuthContext } from '../../../contexts/AuthContext';
 
 export const Detail = () => {
 
     const { id } = useParams();
     const [product, setProduct] = useState({});
     const productService = useService(productServiceFactory);
+    const { userId } = useAuthContext()
 
     useEffect(() => {
         productService.getProduct(id)
@@ -17,7 +19,7 @@ export const Detail = () => {
             })
     }, [id]);
 
-    const owner = true;
+    const owner = userId && product.owner === userId;
 
     return (
         <section className={style["detail__section"]}>
@@ -31,17 +33,19 @@ export const Detail = () => {
                     <p className="prop">Model: <span className="span">{product.model}</span></p>
                     <p className="prop">Description: <span className="span">{product.description}</span></p>
                 </div>
-                <div className={style["detail__button"]}>
-                    {owner && (
-                        <>
-                            <Link to={`/product/edit/${product._id}`} className={`btn ${style["edit"]}`}>Edit</Link>
-                            <Link to={`/product/delete/${product._id}`} className={`btn ${style["delete"]}`}>Delete</Link>
-                        </>
-                    )}
-                    {!owner && (
-                        <Link to={`/cart/${product._id}`} className={`btn ${style["buy"]}`}>Buy Product</Link>
-                    )}
-                </div >
+                {userId && (
+                    <div className={style["detail__button"]}>
+                        {owner && (
+                            <>
+                                <Link to={`/product/edit/${product._id}`} className={`btn ${style["edit"]}`}>Edit</Link>
+                                <Link to={`/product/delete/${product._id}`} className={`btn ${style["delete"]}`}>Delete</Link>
+                            </>
+                        )}
+                        {!owner && (
+                            <Link to={`/cart/${product._id}`} className={`btn ${style["buy"]}`}>Buy Product</Link>
+                        )}
+                    </div >
+                )}
             </article >
         </section >
     );
