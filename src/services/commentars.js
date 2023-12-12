6c52cb72a2d5da2ecf4a836e5c9c136cda895b63
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-12-12 21:33:01 +0200

    creating commentars service with addresses

---
+++ src/services/commentars.js
@@ -0,0 +1,23 @@
+import { requesterFactory } from './requester';
+
+export const commentarServiceFactory = (token) => {
+    const request = requesterFactory(token);
+
+    const getCommentars = async (id) => request.get(`/source/product/${id}/comments`);
+
+    const getComment = async (id, idComment) => request.get(`/source/product/${id}/comment/${idComment}`);
+
+    const createCommentar = async (idProduct, data) => request.post(`/source/product/${idProduct}/comment`, data);
+
+    const editCommentar = async (id, idComment, data) => request.put(`/source/product/${id}/comment/${idComment}`, data);
+
+    const deleteCommentar = async (id, idComment) => request.remove(`/source/product/${id}/comment/${idComment}` + id);
+
+    return {
+        getCommentars,
+        getComment,
+        createCommentar,
+        editCommentar,
+        deleteCommentar,
+    }
+}
