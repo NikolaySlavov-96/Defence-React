44f2dfbfe7ffe254bedda8c4715d205b6ef174e8
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-12-15 20:22:32 +0200

    using in show comment context and change massage

--- src/components/Commentar/ShowComment/ShowComments.js
+++ src/components/Commentar/ShowComment/ShowComments.js
@@ -1,8 +1,6 @@
 import { useParams } from 'react-router-dom';
 import style from './ShowComment.module.css';
 import { useEffect, useState } from 'react';
-import { commentarServiceFactory } from '../../../services/commentars';
-import { useService } from '../../../hooks/useService';
 import { useAuthContext } from '../../../contexts/AuthContext';
 import { useProductContext } from '../../../contexts/ProductContext';
 import { CommentCard } from '../Cards/CommentCard';
@@ -14,15 +12,12 @@ export const ShowAll = () => {
     const { id } = useParams();
     const { userId } = useAuthContext()
     const { getProduct } = useProductContext();
-    const { comment, setComment } = useCommentContext()
+    const { comment, setProdId } = useCommentContext()
     const [ownerProd, setOwnerProd] = useState([]);
-    const CommentarService = useService(commentarServiceFactory);
 
     useEffect(() => {
-        CommentarService.getCommentars(id).then(req => {
-            setComment(req);
-            setOwnerProd(getProduct(id).owner._id)
-        })
+        setProdId(id);
+        setOwnerProd(getProduct(id).owner._id);
     }, [id]);
 
     const owner = userId && ownerProd === userId;
@@ -31,7 +26,7 @@ export const ShowAll = () => {
         <section className={style["comment__section"]}>
             <div>
                 {comment && comment.map(e => <CommentCard key={e._id} {...e} />)}
-                {!comment.length && (<h2>There are no Comments added First.</h2>)}
+                {!comment.length && (<h2>No Comments. You can add first! First.</h2>)}
             </div>
             {userId &&
                 <div>
